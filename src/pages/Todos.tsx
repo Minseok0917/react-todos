1b71de0a01bee449a4b3bdcaeb44b863534f0425
Minseok Jung <37059223+Minseok0917@users.noreply.github.com>
2023-09-08 13:27:24 +0900

    fix: styled-component 에러 글 보고 prefix 추가해서 boolean 으로 해결
    https://mygumi.tistory.com/382

--- src/pages/Todos.tsx
+++ src/pages/Todos.tsx
@@ -161,7 +161,7 @@ export default function Todos() {
           {currentTodos.map((todo: Todo) => (
             <Styled.TodoItem key={todo.id}>
               <Styled.TodoItemCheckBox
-                edit={todo.edit.toString()} // 이거
+                $edit={todo.edit} // 이거
                 onClick={() => handlers.toggleTodoCompleted(todo)}
               >
                 {todo.completed && <BsCheckLg />}
